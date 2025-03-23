6a91b7c86b4c2f85890d95c145cc19e49bfceae2
DavidLee18 <djwodus@gmail.com>
2025-03-23 16:19:19 +0900

    resemble bash behavior: warning on EOF, not error

--- src/fps2_bonus.c
+++ src/fps2_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jaehylee <jaehylee@student.42gyeongsan.kr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 12:55:14 by jaehylee          #+#    #+#             */
-/*   Updated: 2025/03/23 14:56:41 by jaehylee         ###   ########.fr       */
+/*   Updated: 2025/03/23 16:13:50 by jaehylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,22 @@ char	*getln_until(t_list **dyn, char *limit)
 	char	*temp;
 
 	str = "";
-	ft_fprintf(STDOUT_FILENO, ">");
+	ft_fprintf(STDOUT_FILENO, "> ");
 	temp = gc_getline(dyn, STDIN_FILENO);
 	while (temp != NULL && ft_strcmp(gc_strtrim(dyn, temp, "\n"), limit) != 0)
 	{
 		str = gc_strjoin(dyn, str, temp);
-		ft_fprintf(STDOUT_FILENO, ">");
+		ft_fprintf(STDOUT_FILENO, "> ");
 		temp = gc_getline(dyn, STDIN_FILENO);
 	}
-	if (temp == NULL || ft_strcmp(gc_strtrim(dyn, temp, "\n"), limit) != 0)
-		return (ft_fprintf(STDERR_FILENO, "%s: expected \'%s\', got EOF", PIPEX,
-				limit), NULL);
+	if (temp == NULL)
+		return (ft_fprintf(STDERR_FILENO, "%s: expected ", PIPEX),
+			ft_fprintf(STDERR_FILENO, "\'%s\', got EOF\n", limit), str);
+	if (ft_strcmp(gc_strtrim(dyn, temp, "\n"), limit) != 0)
+	{
+		ft_fprintf(STDERR_FILENO, "%s: expected ", PIPEX);
+		ft_fprintf(STDERR_FILENO, "\'%s\', got EOF\n", limit);
+		return (gc_strjoin(dyn, str, temp));
+	}
 	return (str);
 }
